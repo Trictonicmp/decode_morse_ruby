3704f128405cc9add68ad76b137e4e8b1601c691
Trictonicmp <code_shadow@hotmail.com>
2022-07-12 15:26:47 -0500

    Fix linter errors

--- decode.rb
+++ decode.rb
@@ -1,41 +1,28 @@
 def decode_char(str)
   decoder = {
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
-    '--..' => 'Z'
+    '.-' => 'A', '-...' => 'B',
+    '-.-.' => 'C', '-..' => 'D',
+    '.' => 'E', '..-.' => 'F',
+    '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L',
+    '--' => 'M', '-.' => 'N',
+    '---' => 'O', '.--.' => 'P',
+    '--.-' => 'Q', '.-.' => 'R',
+    '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V',
+    '.--' => 'W', '-..-' => 'X',
+    '-.--' => 'Y', '--..' => 'Z'
   }
 
-  return decoder[str]
+  decoder[str]
 end
 
 def decode_str(str)
-  words_arr = str.split("  ")
+  words_arr = str.split('  ')
   phrase = ''
   words_arr.each do |morse_word|
-    morse_word_arr = morse_word.split(' ')
+    morse_word_arr = morse_word.split
     morse_word_arr.each do |morse_char|
       phrase += decode_char(morse_char)
     end
